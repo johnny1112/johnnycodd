6ead3536c58d2ca971910c6fe1de5f64c72b66be
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-01-03 14:03:39 +0530

    Create even.py

---
+++ even.py
@@ -0,0 +1,5 @@
+strating range= int(input("Enter the starting range: "))
+ending range = int(input("Enter the ending range : "))
+for i in range(starting range,ending range+1);
+if(i%2 == 0);
+ print(i);
