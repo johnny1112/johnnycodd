34c46924a489ba2a14c161450a0a199f12ad0e9b
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-01-29 12:11:53 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+char str[10];
+int a,b;
+printf("enter string:");	
+scanf("%s",str);
+n=strlen(str)
+for(b=0;b<n;++)
+if(str[b]>='0' && str[b]<='9')
+printf("yes");
+}
+else
+printf("no");
+}
+return 0;
+}
