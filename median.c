663485b447ae52e74df2afc2a90b065b7b866fbb
johnny1112 <43337475+johnny1112@users.noreply.github.com>
2019-01-29 11:54:07 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,d[10],i,sum=0;
+printf("Enter number ");
+scanf("%d",&a);
+for(i=0;i<;i++)
+{
+scanf("%d",&d[i]);
+sum=sum+d[i];
+}
+int med=sum/a;
+printf("med is %d",med);
+}
